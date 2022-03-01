e21f8234da79a579ee84b3322a13da7c045448b0
Tatsuya Terao <sirokurocat1999@gmail.com>
2022-03-02 01:05:01 +0900

    3/02

---
+++ library/String/PalindromicTree.hpp
@@ -0,0 +1,16 @@
+#ifndef OTERA_EERTREE
+#define OTERA_EERTREE 1
+
+namespace otera {
+    struct PalindromicTree {
+       public:
+
+       private: 
+       struct node {
+           
+       };
+    };
+    
+} // namespace otera
+
+#endif // OTERA_EERTREE
