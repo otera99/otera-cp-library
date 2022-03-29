1b76574bb966d72841fcec185847ff9259580827
Tatsuya Terao <sirokurocat1999@gmail.com>
2022-03-29 23:30:49 +0900

    modify BitwiseConvolution

--- library/Convolution/BitwiseTransformation.hpp
+++ library/Convolution/BitwiseTransformation.hpp
@@ -4,42 +4,32 @@
 namespace otera {
     namespace internal {
         template<typename T> 
-        void subset_zeta_unit_transform(T &a, T &b) {
-            T c = a, d = b;
-            a = c;
-            b = c + d;
+        std::pair<T, T> subset_zeta_unit_transform(std::pair<T, T> a) {
+            return std::make_pair(a.first, a.first + a.second);
         }
 
         template<typename T> 
-        void subset_mobius_unit_transform(T &a, T &b) {
-            T c = a, d = b;
-            a = c;
-            b = -c + d;
+        std::pair<T, T> subset_mobius_unit_transform(std::pair<T, T> a) {
+            return std::make_pair(a.first, -a.first + a.second);
         }
 
         template<typename T> 
-        void supset_zeta_unit_transform(T &a, T &b) {
-            T c = a, d = b;
-            a = c + d;
-            b = d;
+        std::pair<T, T> supset_zeta_unit_transform(std::pair<T, T> a) {
+            return std::make_pair(a.first + a.second, a.second);
         }
 
         template<typename T> 
-        void supset_mobius_unit_transform(T &a, T &b) {
-            T c = a, d = b;
-            a = c - d;
-            b = d;
+        std::pair<T, T> supset_mobius_unit_transform(std::pair<T, T> a) {
+            return std::make_pair(a.first - a.second, a.second);
         }
 
         template<typename T> 
-        void walsh_hadamard_unit_transform(T &a, T &b) {
-            T c = a, d = b;
-            a = c + d;
-            b = c - d;
+        std::pair<T, T> walsh_hadamard_unit_transform(std::pair<T, T> a) {
+            return std::make_pair(a.first + a.second, a.first - a.second);
         }
     } // namespace internal
 
-    template<typename T, std::pair<T, T> (*unit)(std::pair<T, T>)> 
+    template<typename T, std::pair<T, T> (*unit_transform)(std::pair<T, T>)> 
     void kronecker_product(std::vector<T> &x) {
         const int n = (int)x.size();
         // check n is power of 2
@@ -47,38 +37,38 @@ namespace otera {
         for(int bit = 1; bit < n; bit <<= 1) {
             int mask = ~bit;
             for(int i = bit; i < n; i = (i + 1) | bit) {
-                std::pair<T, T> res = unit(x[i & mask], x[i]);
+                std::pair<T, T> res = unit_transform(std::make_pair(x[i & mask], x[i]));
                 x[i & mask] = res.first;
                 x[i] = res.second;
             }
         }
     }
 
-    template<T> subset_zeta(std::vector<T> &x) {
+    template<typename T> void subset_zeta(std::vector<T> &x) {
         kronecker_product<T, internal::subset_zeta_unit_transform<T>>(x);
     }
 
-    template<T> subset_mobius(std::vector<T> &x) {
-        kronecker_product<T, internal::subset_mobiu_unit_transform<T>>(x);
+    template<typename T> void subset_mobius(std::vector<T> &x) {
+        kronecker_product<T, internal::subset_mobius_unit_transform<T>>(x);
     }
 
-    template<T> supset_zeta(std::vector<T> &x) {
-        kronecker_product<T, internal::supset_unit_transform<T>>(x);
+    template<typename T> void supset_zeta(std::vector<T> &x) {
+        kronecker_product<T, internal::supset_zeta_unit_transform<T>>(x);
     }
 
-    template<T> supset_mobius(std::vector<T> &x) {
+    template<typename T> void supset_mobius(std::vector<T> &x) {
         kronecker_product<T, internal::supset_mobius_unit_transform<T>>(x);
     }
 
-    template<T> walsh_hadamard(std::vector<T> &x) {
+    template<typename T> void walsh_hadamard(std::vector<T> &x) {
         kronecker_product<T, internal::walsh_hadamard_unit_transform<T>>(x);
     }
 
-    template<T> walsh_hadamard_inv(std::vector<T> &x) {
+    template<typename T> void walsh_hadamard_inv(std::vector<T> &x) {
         const int n = (int)x.size();
         kronecker_product<T, internal::walsh_hadamard_unit_transform<T>>(x);
         for(auto &val: x) val /= n;
     }
 } // namespace otera
 
-#endif OTERA_BITWISE_TRANSFORMATION
+#endif // OTERA_BITWISE_TRANSFORMATION
