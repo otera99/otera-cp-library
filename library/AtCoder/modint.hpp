588386b15c039dbed08c76564c90a1305e783a0a
Tatsuya Terao <sirokurocat1999@gmail.com>
2022-03-29 09:25:41 +0900

    add modint

--- library/AtCoder/modint.hpp
+++ library/AtCoder/modint.hpp
@@ -3,6 +3,12 @@
 
 #include<atcoder/modint>
 
+namespace otera {
+    using modint107 = atcoder::modint1000000007;
+    using modint998 = atcoder::modint998244353;
+    using modint = atcoder::modint;
+}; //namespace otera
+
 std::ostream& operator<<(std::ostream& out, const atcoder::modint1000000007 &e) {
     out << e.val();
     return out;
