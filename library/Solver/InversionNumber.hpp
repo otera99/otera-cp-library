7dc2ab330b03badd11da9390628a4186973710ec
Tatsuya Terao <sirokurocat1999@gmail.com>
2022-09-25 01:06:52 +0900

    add InversionNumber.hpp

---
+++ library/Solver/InversionNumber.hpp
@@ -0,0 +1,22 @@
+#ifndef OTERA_INVERSION_NUMBER_HPP
+#define OTERA_INVERSION_NUMBER_HPP 1
+
+#include<atcoder/fenwicktree>
+#include<library/Util/Compressor.hpp>
+
+namespace otera {
+    long long InversionNumber(std::vector<long long> a) {
+        int _n = (int)a.size();
+        Compressor<long long> ca(a);
+        int sz = (int)ca.size();
+        atcoder::fenwick_tree<long long> bit(sz);
+        long long res = 0;
+        for(int i = 0; i < _n; ++ i) {
+            res += bit.sum(ca.get(a[i]) + 1, sz);
+            bit.add(ca.get(a[i]), 1);
+        }
+        return res;
+    }
+} // namespace otera
+
+#endif // OTERA_INVERSION_NUMBER_HPP
