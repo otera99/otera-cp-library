5aa4ba603e98fd52ae440686780fe1bab0932996
Tatsuya Terao <sirokurocat1999@gmail.com>
2022-10-01 04:08:41 +0900

    modify test

--- test/src/Tree/LCA.test.cpp
+++ test/src/Tree/LCA.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/lca"
 
-#include"library/Tree/LCA.hpp"
 #include<bits/stdc++.h>
+#include"library/Tree/LCA.hpp"
 using namespace std;
 
 using ll = long long;
