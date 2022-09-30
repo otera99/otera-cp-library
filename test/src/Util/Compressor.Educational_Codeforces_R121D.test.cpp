ace9ab8942c417a867668797f0e7b5cff648cf62
Tatsuya Terao <sirokurocat1999@gmail.com>
2022-10-01 04:02:54 +0900

    modify tests

--- test/src/Util/Compressor.Educational_Codeforces_R121D.test.cpp
+++ test/src/Util/Compressor.Educational_Codeforces_R121D.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://codeforces.com/contest/1626/problem/D"
 #define VERIFY "https://codeforces.com/contest/1626/submission/144768069"
-#define IGNORE 1
+#define IGNORE
 #include<bits/stdc++.h>
 #include<library/Util/Compressor.hpp>
 // #include<otera/Compressor>
